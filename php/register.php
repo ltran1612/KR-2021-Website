59d13ddaeb01b5da38de429464cde72f6400ce94
ltran <longdtranw@gmail.com>
2021-07-28 10:42:01 -0600

    change background of register.php to gray

--- php/register.php
+++ php/register.php
@@ -289,7 +289,7 @@
 </head>
 
 <body>
-    <div class="page-wrapper p-t-45 p-b-50">
+    <div class="page-wrapper bg-kr-gray p-t-45 p-b-50">
         <div class="wrapper wrapper--w790">
             <div class="card card-5">
                 <div class="card-heading bg-blue">
