db85a29a7cb05eccbb927618e1463dcecc874ff0
ltran <longdtranw@gmail.com>
2021-08-04 14:00:53 -0600

    change resources path

--- php/edit_page.php
+++ php/edit_page.php
@@ -32,7 +32,7 @@
 
 <head>
     <!-- Jquery JS-->
-    <script src="/vendor/jquery/jquery.min.js"></script>
+    <script src="../vendor/jquery/jquery.min.js"></script>
 
     <!-- Required meta tags-->
     <meta charset="UTF-8">
@@ -45,17 +45,17 @@
     <title>EDIT SUBMISSION</title>
 
     <!-- Icons font CSS-->
-    <link href="/vendor/mdi-font/css/material-design-iconic-font.min.css" rel="stylesheet" media="all">
-    <link href="/vendor/font-awesome-4.7/css/font-awesome.min.css" rel="stylesheet" media="all">
+    <link href="../vendor/mdi-font/css/material-design-iconic-font.min.css" rel="stylesheet" media="all">
+    <link href="../vendor/font-awesome-4.7/css/font-awesome.min.css" rel="stylesheet" media="all">
     <!-- Font special for pages-->
     <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i,800,800i" rel="stylesheet">
 
     <!-- Vendor CSS-->
-    <link href="/vendor/select2/select2.min.css" rel="stylesheet" media="all">
-    <link href="/vendor/datepicker/daterangepicker.css" rel="stylesheet" media="all">
+    <link href="../vendor/select2/select2.min.css" rel="stylesheet" media="all">
+    <link href="../vendor/datepicker/daterangepicker.css" rel="stylesheet" media="all">
 
     <!-- Main CSS-->
-    <link href="/css/main.css" rel="stylesheet" media="all">
+    <link href="../css/main.css" rel="stylesheet" media="all">
 </head>
 
 <body>
@@ -254,15 +254,15 @@
     </div>
 
     <!--Custom Stylesheet-->
-    <link href="/css/my.css" rel="stylesheet" media="all">
+    <link href="../css/my.css" rel="stylesheet" media="all">
     
     <!--Vendor JS-->
-    <script src="/vendor/select2/select2.min.js"></script>
-    <script src="/vendor/datepicker/moment.min.js"></script>
-    <script src="/vendor/datepicker/daterangepicker.js"></script>
+    <script src="../vendor/select2/select2.min.js"></script>
+    <script src="../vendor/datepicker/moment.min.js"></script>
+    <script src="../vendor/datepicker/daterangepicker.js"></script>
    
     <!-- Main JS-->
-    <script src="/js/global.js"></script>
+    <script src="../js/global.js"></script>
 </body><!-- This was made based on a template of Colorlib (https://colorlib.com) -->
 
 </html>
