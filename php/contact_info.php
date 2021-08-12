1984f8def561f9a02460ce16e6234a092896b200
ltran <longdtranw@gmail.com>
2021-08-12 10:55:19 -0600

    update the email address for contact info

--- php/contact_info.php
+++ php/contact_info.php
@@ -1,6 +1,6 @@
 <div class="contact-div font-poppins">
     <div>
         <h4>CONTACT</h4>
-        Please contact us at ... for any questions that you may have.
+        Please contact us at longtran@nmsu.edu for any questions that you may have.
     </div>
 </div>
