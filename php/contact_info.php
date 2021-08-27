582b20e357a9d6f4aec3ae1b8dec158d483c8caf
ltran <longdtranw@gmail.com>
2021-08-27 14:19:13 -0600

    update email contact

--- php/contact_info.php
+++ php/contact_info.php
@@ -1,6 +1,6 @@
 <div class="contact-div font-poppins">
     <div>
         <h4>CONTACT</h4>
-        Please contact us at <b style="color: orange">kr21virtual@gmail.com</b> for any questions that you may have.
+        Please contact us at <b style="color: orange">kr21virtual+registration@gmail.com</b> for any questions that you may have.
     </div>
 </div>
